94090a3ad1f621ef6904d3d5e547db150a2bd1a1
Larissa Perinoto <perinotolarissa@gmail.com>
2022-11-18 17:42:17 -0300

    Cria a interface Fighter.

--- src/Races/Halfling.ts
+++ src/Races/Halfling.ts
@@ -1,16 +1,20 @@
 import Race from './Race';
 
 export default class Halfling extends Race {
-  public maxLifePoints: number;
-  public static instances = 0;
+  private _maxLifePoints: number;
+  private static _instances = 0;
 
   constructor(name: string, dexterity: number) {
     super(name, dexterity);
-    this.maxLifePoints = 60;
+    this._maxLifePoints = 60;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
   }
 
   static createdRacesInstances(): number {
-    this.instances += 1;
-    return this.instances;
+    this._instances += 1;
+    return this._instances;
   }
 }
