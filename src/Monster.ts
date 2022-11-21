c7ab62482d32a2704c06f6b96faaf5ae8e8e8853
Larissa Perinoto <perinotolarissa@gmail.com>
2022-11-21 17:09:23 -0300

    Cria a classe Monster.

---
+++ src/Monster.ts
@@ -0,0 +1,36 @@
+import Fighter, { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  public get lifePoints() : number {
+    return this._lifePoints;
+  }
+
+  public get strength(): number {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = this._lifePoints - attackPoints;
+    if (attackPoints > 0) {
+      this._lifePoints -= damage;
+    }
+
+    if (this._lifePoints <= 0) {
+      this._lifePoints = -1;
+    }
+
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter | Fighter): void {
+    enemy.receiveDamage(this.strength);
+  }
+}
